5a159b105f77b3f021882dae2b85ab552a3092ae
dyadyasawa <dyadyasawa@gmail.com>
2024-05-14 14:22:20 +0300

    Создание и применение миграций.

--- mailings/models.py
+++ mailings/models.py
@@ -2,6 +2,8 @@ from django.utils import timezone
 
 from django.db import models
 
+from users.models import User
+
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -10,7 +12,7 @@ class Client(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=50, verbose_name='Имя')
     comment = models.TextField(verbose_name='Примечание', **NULLABLE)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Вдаделец', **NULLABLE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Вдаделец', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} ({self.email})'
@@ -25,7 +27,7 @@ class Message(models.Model):
 
     title = models.CharField(max_length=50, verbose_name='Тема сообщения', **NULLABLE)
     message = models.TextField(verbose_name='Сообщение')
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Вдаделец', **NULLABLE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Вдаделец', **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -61,7 +63,7 @@ class Mailing(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='Сообщение')
     clients = models.ManyToManyField(Client, verbose_name='Клиенты')
 
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Вдаделец', **NULLABLE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Вдаделец', **NULLABLE)
 
     def __str__(self):
         return f'{self.time_start} ({self.period})'
@@ -87,7 +89,7 @@ class Log(models.Model):
 
     mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='Рассылка')
 
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='Вдаделец', **NULLABLE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Вдаделец', **NULLABLE)
 
     class Meta:
 
---
+++ users/urls.py
@@ -0,0 +1,23 @@
+
+from users.forms import LoginCustomForm
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from users.apps import UsersConfig
+from users.views import UserCreateView
+
+app_name = UsersConfig.name
+
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users_app/login.html', form_class=LoginCustomForm), name="login"),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('register/', UserCreateView.as_view(), name='register'),
+    # path('register/email_confirm/<str:token>/', email_verification, name='email_confirm'),
+    # path('register/message/', RegisterMessageView.as_view(), name='register_message'),
+    #
+    # path('password_recovery/', PasswordRecoveryView.as_view(), name='password_recovery'),
+    # # path('password_recovery/create_new_password/<str:code>', create_new_password, name='create_new_password'),
+    # path('password_recovery/message/', PasswordRecoveryMessageView.as_view(), name='recovery_message')
+]
